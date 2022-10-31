8e518222c2a0366a64b19c51e8db345b9d2c6715
hitochan777 <hitochan777@gmail.com>
2022-11-01 02:48:43 +0900

    Update bs.rb

--- Formula/bs.rb
+++ Formula/bs.rb
@@ -1,7 +1,7 @@
 class Bs < Formula
   desc "CLI for bson/json converter"
   homepage "https://github.com/hitochan777/bs"
-  url "https://github.com/hitochan777/bs/archive/refs/tags/0.1.0.tar.gz"
+  url "https://github.com/hitochan777/bs/releases/download/0.1.0/bs-0.1.0-x86_64-linux.tar.gz"
   sha256 "dd0db09b509f12061a8e6718db125c040c12b9a01207b0ad4d6bee927b367cd7"
   version "0.1.0"
 
