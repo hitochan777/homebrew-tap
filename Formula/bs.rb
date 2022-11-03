886807b8469a65710721ae1bc916b079e3fbf098
hitochan777 <hitochan777@gmail.com>
2022-11-03 15:58:08 +0900

    Update bs.rb

--- Formula/bs.rb
+++ Formula/bs.rb
@@ -3,7 +3,7 @@ class Bs < Formula
   homepage "https://github.com/hitochan777/bs"
   url "https://github.com/hitochan777/bs/releases/download/0.3.0/bs-0.3.0-x86_64-linux.tar.gz"
   sha256 "8fa6d739885c681c1e62a16e5193e1615ae72e7001329515925862c829e3b2d2"
-  version "0.2.0"
+  version "0.3.0"
 
   def install
     bin.install "bs"
