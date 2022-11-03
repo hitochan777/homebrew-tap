f4ee71a2be967f58c2551ae9a3ad95ccff4ae4fc
hitochan777 <hitochan777@gmail.com>
2022-11-03 18:43:23 +0900

    Update bjc.rb

--- Formula/bjc.rb
+++ Formula/bjc.rb
@@ -2,7 +2,7 @@ class Bjc < Formula
   desc "CLI for bson/json converter"
   homepage "https://github.com/hitochan777/bjc"
   url "https://github.com/hitochan777/bjc/releases/download/0.4.0/bjc-0.4.0-x86_64-linux.tar.gz"
-  sha256 "b8d6ed1f065f59f08080499316589106feb92b896647a2849224935e43f0a845"
+  sha256 "3f4461d5f51c28629e330710eb3bd48933be3ea710019858a806230164292d22"
   version "0.4.0"
 
   def install
