f97d2f495fff218e66f83f9d049a40332a38f484
hitochan777 <hitochan777@gmail.com>
2024-09-23 16:29:42 +0900

    Update watashh.rb

--- Formula/watashh.rb
+++ Formula/watashh.rb
@@ -1,7 +1,7 @@
 class Watashh < Formula
   desc "Watashh"
   homepage "https://github.com/hitochan777/homebrew-tap"
-  url "https://github.com/hitochan777/homebrew-tap/releases/download/0.1.0/watashh-0.1.0-x86_64-linux.tar.gz"
+  url "https://github.com/hitochan777/homebrew-tap/releases/download/watashh-0.1.0/watashh-0.1.0-x86_64-linux.tar.gz"
   sha256 "d63ef212e10af96b796e2d88643a096c9c1d9702cbc740f3b06b4d5fb3db993e"
   version "0.1.0"
 
